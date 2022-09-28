1a2847826d0aa45d3665782786ce668633d8223c
Francisco Crema <francrema00@gmail.com>
2022-09-28 12:18:11 -0300

    primer commit

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,14 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faCartShopping} from '@fortawesome/free-solid-svg-icons'
+
+const Icon = () =>{
+    return (
+        <>
+        <FontAwesomeIcon icon= {faCartShopping} />
+        </>
+
+
+    )
+}
+
+export default Icon;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import "./navbar.css";
+
+import CartWidget from '../CartWidget'
+
+function NavBar() {
+  return (
+    <>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home" id="logo">
+            Lauvi
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#features" id="color">
+                Inicio
+              </Nav.Link>
+              <Nav.Link href="#pricing" id="color">
+                Productos
+              </Nav.Link>
+              <Nav.Link href="#pricing" id="color">
+                Contacto
+              </Nav.Link>
+              <Nav.Link href="#pricing" id="color">
+                Contacto
+              </Nav.Link>
+            </Nav>
+
+            <Nav>
+              <Nav.Link id="icono">
+                <CartWidget />
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default NavBar;
