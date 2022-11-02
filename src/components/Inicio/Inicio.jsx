1edb9df9cecea819f017ffa6e61787e9fd45a681
Francisco Crema <francrema00@gmail.com>
2022-11-02 16:38:36 -0300

    Agregado de algunos productos en el inicio

--- src/components/Inicio/Inicio.jsx
+++ src/components/Inicio/Inicio.jsx
@@ -1,13 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { collection, getDocs, limit, query, where } from "firebase/firestore";
+import { baseDeDatos } from "../../servicios/fireBaseConfig";
+import ItemList from "../Main/ItemList";
+import "../Main/item.css";
 
-const Inicio = ({saludo}) => {
+function Inicio() {
+  const [items, setItems] = useState([]);
+  const getData = async () => {
+    try {
+      const document1 = query(
+        collection(baseDeDatos, "productos"),
+        where("categoria", "==", "buzo"),
+        limit(3)
+      );
+
+      const col1 = await getDocs(document1);
+      const result1 = col1.docs.map(
+        (doc) => (doc = { id: doc.id, ...doc.data() })
+      );
+
+      const document2 = query(
+        collection(baseDeDatos, "productos"),
+        where("categoria", "==", "camisetas"),
+        limit(3)
+      );
+
+      const col2 = await getDocs(document2);
+      const result2 = col2.docs.map(
+        (doc) => (doc = { id: doc.id, ...doc.data() })
+      );
+
+      setItems([...result1, ...result2]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
-    <main>
-    <div className="item-list-container">
-      <h3 className="text"> {saludo} </h3>
-    </div>
-  </main>
-  )
+    <>
+      <ItemList items={items} />
+    </>
+  );
 }
 
-export default Inicio
+export default Inicio;
